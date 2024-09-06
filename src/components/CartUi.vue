274da708b760f34b44f42d1df60a1347aca660d8
Sky Blue <dawadiaakash@gmail.com>
2024-09-06 11:33:31 +0545

    Feat: Tailwind flowbite card initialize

--- src/components/CartUi.vue
+++ src/components/CartUi.vue
@@ -1,6 +1,41 @@
 <template>
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello from </h1>
+    <div class="grid grid-cols-4 gap-4">
+      <div
+        v-for="n in 16" :key="n"
+        class="mt-20 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img
+            class="p-8 rounded-t-lg"
+            src="https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg"
+            alt="Burger"
+          />
+        </a>
+        <div class="px-5 pb-5">
+          <a href="#">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Spicy KFC Ham Burger with double cheese
+            </h5>
+          </a>
+          <div class="flex items-center mt-2.5 mb-5">
+            <div class="flex items-center space-x-1 rtl:space-x-reverse">
+              <!-- Star ratings -->
+              <svg class="w-5 h-5 text-yellow-400" aria-hidden="true" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M10.979 1.686c.437-1.098 1.91-1.098 2.347 0l2.056 5.166 5.384.78c1.201.174 1.681 1.65.81 2.497l-3.898 3.8.92 5.363c.211 1.232-1.083 2.165-2.202 1.586l-4.81-2.53-4.81 2.53c-1.119.579-2.413-.354-2.202-1.586l.92-5.363-3.898-3.8c-.872-.847-.391-2.323.81-2.497l5.384-.78L10.979 1.686z"/>
+              </svg>
+              <!-- Repeat the stars as needed -->
+            </div>
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded ms-3">5.0</span>
+          </div>
+          <div class="flex items-center justify-between">
+            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+            <a
+              href="#"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </template>
   
