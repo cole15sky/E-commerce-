81c2fa4bd556bd29f026938517ffc11b8f7452a4
Sky Blue <dawadiaakash@gmail.com>
2024-09-06 10:33:34 +0545

    Fetch: Tailwind css

---
+++ src/components/CartUi.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello from </h1>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  
